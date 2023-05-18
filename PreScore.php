6c7dd6db51cc1df570e1423de2a3ab46e4d41ff9
StartledHamster <110441979+StartledHamster@users.noreply.github.com>
2023-05-18 22:02:02 +1000

    Update PreScore.php

--- PreScore.php
+++ PreScore.php
@@ -10,9 +10,37 @@
   </head>
   <body>
  	  <?php include 'includes/header.inc'; ?>
-	    require_once "settings.php";
-	     $conn = @mysqli_connect($host,$user,$pwd,$sql_db);
-    
-
+	  <?php
+	  	session_start();
+	  	
+	    	require_once "settings.php";
+	     	$conn = @mysqli_connect($host,$user,$pwd,$sql_db);
+		if (!$conn) {
+			// Displays an error message
+			echo "<p>Database connection failure</p>";
+	    	} 
+	    	else {
+			$query = "SELECT * FROM `CompetitionEnrolments` WHERE `archer_id` = '$archer_id' "; //need login id global variable
+			$enrolments =  mysqli_query($conn, $query);
+		}	
+	  ?>
+		
+	  <form method="POST" action="ScoreSheet.php"  novalidate="novalidate">
+        <label>Select Competition</label>
+        <select name="Competition">
+            <?php
+                while ($competition = mysqli_fetch_array(
+                        $enrolments,MYSQLI_ASSOC)):;
+            ?>
+                <option value="<?php echo $competition["competition_id"]; ?>">
+                    <?php echo $competition["competition_id"]; ?>
+                </option>
+            <?php
+                endwhile;
+            ?>
+        </select>
+        <br>
+        <input type="submit" value="submit" name="submit">
+    </form>
   </body>
 </html>
