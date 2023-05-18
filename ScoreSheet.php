7ee422daa0d9386ef0b1dbb9d28e5b1f5d03ae36
StartledHamster <110441979+StartledHamster@users.noreply.github.com>
2023-05-18 21:25:45 +1000

    Create ScoreSheet.php

---
+++ ScoreSheet.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+  <html lang = "en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="description" content="ScoreSheet" />
+    <meta name="keywords" 	 content="HTML, CSS" />
+    <meta name="author"		 content="Halim" />
+    <link rel = "stylesheet" href ="styles/style.css">
+    <title> Score Sheet </title>
+  </head>
+  <body>
+ 	  <?php include 'includes/header.inc'; ?>
+    
+
+  </body>
+</html>
